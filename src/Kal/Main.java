7183266cad3ab7d819cea920fb9449796452d11e
Wojtek <w.charasinski@yahoo.com>
2025-01-31 21:29:26 +0100

    Final

--- src/Kal/Main.java
+++ src/Kal/Main.java
@@ -4,18 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    private JFrame frame;
-    private JTextField textField;
-    private String operator;
-    private double num1, num2, result;
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            try {
-                Kalkulator window = new Kalkulator();
-                window.frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
+        SwingUtilities.invokeLater( new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Kalkulator window = new Kalkulator();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
